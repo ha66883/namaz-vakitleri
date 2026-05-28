c39fa44a3fea3028d4e999edc3429e3d833cb60f
Havva Demir <havva.demir.43@outlook.de>
2026-05-28 10:42:56 +0200

    -85

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -202,12 +202,12 @@
     // 1. 🍏 iOS / Safari Check
     if ("webkitCompassHeading" in event) {
       const rawHeading = (event as any).webkitCompassHeading;
-      currentHeading = (360 - rawHeading + 190 + 360) % 360;
+      currentHeading = (360 - rawHeading + 80 + 360) % 360;
     }
     // 2. 🤖 Android / Chrome Check (Mit expliziter Typprüfung auf null!)
     else if (event.alpha !== null) {
       // TypeScript weiß jetzt zu 100%, dass event.alpha eine Zahl ist. Das Rot verschwindet!
-      currentHeading = (event.alpha - 80 + 360) % 360;
+      currentHeading = (event.alpha - 85 + 360) % 360;
     } else {
       // Sensor liefert keine brauchbaren Daten
       return;
