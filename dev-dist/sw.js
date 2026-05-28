95fc03c59099cfd3437fdc3f3fb5a500e732ceb5
Havva Demir <havva.demir.43@outlook.de>
2026-05-28 09:04:32 +0200

    delete kible yönü

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.if8aclrgl8"
+    "revision": "0.o9c4dr8ks2o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
