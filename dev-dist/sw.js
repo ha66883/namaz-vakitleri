7fa2a56ef4355ec5490a83a012462bde7030309d
Havva Demir <havva.demir.43@outlook.de>
2026-06-03 11:32:35 +0200

    encodeuri

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/",
-    "revision": "0.icmgdtrobkg"
+    "revision": "0.vcgamp0cmbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/"), {
--- src/routes/api/location-search/+server.ts
+++ src/routes/api/location-search/+server.ts
@@ -1,13 +1,34 @@
 import { json } from "@sveltejs/kit";
 
 export async function GET({ url }) {
-  const city = url.searchParams.get("city");
+  const city = url.searchParams.get("city") ?? "";
+  const normalizedCity = normalizeTurkish(city);
+
+  // const response = await fetch(
+  //   `https://prayertimes.api.abdus.dev/api/diyanet/search?q=${city}`,
+  // );
 
   const response = await fetch(
-    `https://prayertimes.api.abdus.dev/api/diyanet/search?q=${city}`,
+    `https://prayertimes.api.abdus.dev/api/diyanet/search?q=${encodeURIComponent(normalizedCity)}`,
   );
 
   const data = await response.json();
 
   return json(data);
+}
+
+function normalizeTurkish(text: string) {
+  return text
+    .replace(/ç/g, "c")
+    .replace(/Ç/g, "C")
+    .replace(/ğ/g, "g")
+    .replace(/Ğ/g, "G")
+    .replace(/ı/g, "i")
+    .replace(/İ/g, "I")
+    .replace(/ö/g, "o")
+    .replace(/Ö/g, "O")
+    .replace(/ş/g, "s")
+    .replace(/Ş/g, "S")
+    .replace(/ü/g, "u")
+    .replace(/Ü/g, "U");
 }
